f589e0b843b180238d61324afdbd843a05b8e707
Onnur <72163294+Onnur@users.noreply.github.com>
2020-10-05 14:30:59 +0200

    Ändringar i Account class

--- InternationalFoodAB/InternationalFoodAB/Classes/Account.cs
+++ InternationalFoodAB/InternationalFoodAB/Classes/Account.cs
@@ -8,5 +8,42 @@ namespace InternationalFoodAB
 {
     public abstract class Account
     {
+        //Properties
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        //Constructor overloadar
+        public Account()
+        {
+
+        }
+
+        //Constructor med 2 parameter
+        public Account(string UserName, string Password)
+        {
+            this.UserName = UserName;
+            this.Password = Password;
+        }
+
+        //Constructor med 3 parameter
+        public Account(string UserName, string Password, string Email)
+        {
+            this.UserName = UserName;
+            this.Password = Password;
+            this.Email = Email;
+        }
+
+        public void AdminLogin()
+        {
+            if (UserName != "Admin" && Password != "King123")
+            {
+                //Messagebox visar att inloggningen är felaktig.
+            }
+        }
+
+
+
+
     }
 }
