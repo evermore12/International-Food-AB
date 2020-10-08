9c8a6a51e004673c8703a41e5c594a5f19f5c5d1
Onnur <72163294+Onnur@users.noreply.github.com>
2020-10-08 09:42:17 +0200

    logga ut

--- InternationalFoodAB/InternationalFoodAB/frmMainForm.cs
+++ InternationalFoodAB/InternationalFoodAB/frmMainForm.cs
@@ -24,6 +24,22 @@ namespace InternationalFoodAB
 
         private void cmdLogIn_Click(object sender, EventArgs e)
         {
+            //   Account account = new Account();
+            //  account.CheckInlog();
+
+
+            //Logga ut funktion
+            if (IsLoggedIn)
+            {
+                //    ucLogIn uclogin1 = new ucLogIn(this);
+                //   uclogin1.Hide();
+                IsLoggedIn = false;
+                lblAccount.Text = "Ej inloggad";
+                cmdLogIn.Text = "Logga in";
+                //      cmdLogIn_Click = CheckIn
+                //     lblAccount
+            }
+
             ucLogIn logInContron = new ucLogIn(this);
             this.Controls.Add(logInContron);
             logInContron.Show();
@@ -32,6 +48,7 @@ namespace InternationalFoodAB
 
         public void LoggedIn(string email)
         {
+            cmdLogIn.Text = "Logga ut";
             lblAccount.Text = "Inloggad som: " + email;
         }
 
